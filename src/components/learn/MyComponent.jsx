d89d22c00960fcf11ba245a6af4d7387b5dde7a3
nguyenvinh9x93@gmail.com <ngocquynh@email.com>
2025-12-24 22:02:44 +0700

    add css in component

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,7 +1,14 @@
-//component = html + css + js
+//JSX
+import './style.css';
+
 const MyComponent = () => {
     return (
-        <div>Eric & Sky</div>
+        <>
+            <div>Eric & Sky</div>
+            <div className="child"
+                style={{ borderRadius: "10px" }}
+            >child</div>
+        </>
     );
 }
 
