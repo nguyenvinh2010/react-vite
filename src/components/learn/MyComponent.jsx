74d01a8438a107bebbdfa2bc5f4ba76fba1eed9d
nguyenvinh9x93@gmail.com <ngocquynh@email.com>
2025-12-24 22:15:31 +0700

    css in component

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -2,11 +2,20 @@
 import './style.css';
 
 const MyComponent = () => {
+    //const bien = "Eric";
+    const bien = [1, 2, 3];
+    // const bien = {
+    //     name: "V",
+    //     age: 3
+    // };
     return (
         <>
-            <div>Eric & Sky</div>
+            <div>{JSON.stringify(bien)} & Sky</div>
+            <div>{console.log(">>>ERIC")}</div>
             <div className="child"
-                style={{ borderRadius: "10px" }}
+                style={
+                    { borderRadius: "10px" }
+                }
             >child</div>
         </>
     );
