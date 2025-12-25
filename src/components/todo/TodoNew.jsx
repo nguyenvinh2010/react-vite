5bc4d48f10c10093c17e6dafcbee7b3c2cfbc653
nguyenvinh9x93@gmail.com <ngocquynh@email.com>
2025-12-25 21:40:40 +0700

    event

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -3,11 +3,22 @@ const TodoNew = (props) => {
     console.log(">>>>check props", props)
     const { addNewToDo } = props;
     //addNewToDo("Henry")
+    const handleClick = () => {
+        alert("click me")
+    }
+    const handleOnchange = (name) => {
+        console.log(">>>>handleOnChange", name)
+    }
     return (
         <div className='todo-new'>
-            <input type="text"></input>
-            <button>Add</button>
-        </div>
+            <input type="text"
+                onChange={(event) => handleOnchange(event.target.value)}
+            ></input>
+            <button
+                style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add</button>
+        </div >
     )
 }
 export default TodoNew;
